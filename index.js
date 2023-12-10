3d91e47edf1ea3fa961bfd049a5b71a4a020c221
jonathanwpaul <paulwjonathan@gmail.com>
2023-12-10 15:59:40 -0500

    updated to run in console

--- index.js
+++ index.js
@@ -1,143 +1,148 @@
-import puppeteer from "puppeteer";
-import fs from 'fs';
+import puppeteer from 'puppeteer'
+import fs from 'fs'
 import { stringify } from 'csv-stringify'
+import readline from 'readline'
 
-// to get the list of urls, run a search on apartments.com and use this in the console:
-//   links = JSON.stringify(Array.from(document.querySelectorAll('.property-information > .property-link')).map(each => each.href))
-//import apartmentURLs from "./apartmentURLs.json" assert { type: "json" };
+async function getURLs(searchURL) {
+	const browser = await puppeteer.launch({
+		headless: false,
+		defaultViewport: null,
+	})
+	const page = await browser.newPage()
+	await page.goto(searchURL, {
+		waitUntil: 'networkidle0',
+	})
 
-async function getURLs(URL) {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-  const page = await browser.newPage();
-  await page.goto(URL, {
-    waitUntil: "domcontentloaded",
-  });
+	let apartmentURLs = await page.evaluate(() =>
+		Array.from(
+			document.querySelectorAll('.property-information > .property-link')
+		).map((each) => each.href)
+	)
+	await browser.close()
 
-  let URLs = await page.evaluate(() =>
-    Array.from(
-      document.querySelectorAll(".property-information > .property-link")
-    ).map((each) => each.href)
-  );
-  await browser.close();
-
-  return URLs;
+	return apartmentURLs
 }
 
-async function getData(URL, fieldList) {
-  // Start a Puppeteer session with:
-  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
+async function getData(apartmentURL) {
+	// Start a Puppeteer session with:
+	// - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+	// - no default viewport (`defaultViewport: null` - website page will in full width and height)
+	const browser = await puppeteer.launch({
+		headless: false,
+		defaultViewport: null,
+	})
 
-  // Open a new page
-  const page = await browser.newPage();
+	// Open a new page
+	const page = await browser.newPage()
 
-  // On this new page:
-  // - open the URL
-  // - wait until the dom content is loaded (HTML is ready)
-  await page.goto(URL, {
-    waitUntil: "domcontentloaded",
-  });
+	// On this new page:
+	// - open the URL
+	// - wait until the dom content is loaded (HTML is ready)
+	await page.goto(apartmentURL, {
+		waitUntil: 'domcontentloaded',
+	})
 
-  const apartmentDetails = await page.evaluate(() => {
-    const _querySelector = (node, query) =>
-      (node.querySelector(query)?.innerText ?? "")
-        .replace(/[\r\n\s]+/g, " ")
-        .trim();
+	const apartmentDetails = await page.evaluate(() => {
+		const _querySelector = (node, query) =>
+			(node.querySelector(query)?.innerText ?? '')
+				.replace(/[\r\n\s]+/g, ' ') //whitespace cleaning
+				.trim()
 
-    let unitTypesNodes = document.querySelectorAll(".pricingGridItem");
-    return Array.from(unitTypesNodes).map((e, i) => {
-      availableUnits = unitTypesNodes[i].querySelectorAll("li.unitContainer");
-      return Array.from(availableUnits).map((e, j) => {
-        return {
-          name: _querySelector(document, "#propertyName"),
-          address: _querySelector(document, "#propertyAddressRow"),
-          reviews: _querySelector(document, "#propertyReviewRow"),
-          displayName: _querySelector(unitTypesNodes[i], ".modelName"),
-          beds: _querySelector(
-            unitTypesNodes[i],
-            ".detailsTextWrapper span:nth-child(1)"
-          ).split(" ")[0],
-          baths: _querySelector(
-            unitTypesNodes[i],
-            ".detailsTextWrapper span:nth-child(2)"
-          ).split(" ")[0],
-          unit: _querySelector(
-            availableUnits[j],
-            ".unitColumn .unitBtn span:not(.screenReaderOnly)"
-          ),
-          rent: _querySelector(
-            availableUnits[j],
-            ".pricingColumn span:not(.screenReaderOnly)"
-          ),
-          discount: _querySelector(document, "#rentSpecialsSection p"),
-          // dogFee:,
-          // dogRent:,
-          // adminFee:,
-          // applicationFee:,
-          sqft: _querySelector(
-            availableUnits[j],
-            ".sqftColumn span:not(.screenReaderOnly)"
-          ),
-          availability: _querySelector(
-            availableUnits[j],
-            ".availableColumn span.dateAvailable"
-          ),
-        };
-      });
-    });
-  });
+		let unitTypesNodes = document.querySelectorAll('.pricingGridItem')
+		return Array.from(unitTypesNodes).map((e, i) => {
+			availableUnits =
+				unitTypesNodes[i].querySelectorAll('li.unitContainer')
+			return Array.from(availableUnits).map((e, j) => {
+				return {
+					url: apartmentURL,
+					name: _querySelector(document, '#propertyName'),
+					address: _querySelector(document, '#propertyAddressRow'),
+					reviews: _querySelector(document, '#propertyReviewRow'),
+					displayName: _querySelector(
+						unitTypesNodes[i],
+						'.modelName'
+					),
+					beds: _querySelector(
+						unitTypesNodes[i],
+						'.detailsTextWrapper span:nth-child(1)'
+					).split(' ')[0],
+					baths: _querySelector(
+						unitTypesNodes[i],
+						'.detailsTextWrapper span:nth-child(2)'
+					).split(' ')[0],
+					unit: _querySelector(
+						availableUnits[j],
+						'.unitColumn .unitBtn span:not(.screenReaderOnly)'
+					),
+					rent: _querySelector(
+						availableUnits[j],
+						'.pricingColumn span:not(.screenReaderOnly)'
+					),
+					discount: _querySelector(
+						document,
+						'#rentSpecialsSection p'
+					),
+					// dogFee:,
+					// dogRent:,
+					// adminFee:,
+					// applicationFee:,
+					sqft: _querySelector(
+						availableUnits[j],
+						'.sqftColumn span:not(.screenReaderOnly)'
+					),
+					availability: _querySelector(
+						availableUnits[j],
+						'.availableColumn span.dateAvailable'
+					),
+				}
+			})
+		})
+	})
 
-  // Close the browser
-  await browser.close();
-  return apartmentDetails;
+	// Close the browser
+	await browser.close()
+	return apartmentDetails
 }
 
-function convertToCSV(objArray) {
-  var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
-  var str = "";
-
-  for (var i = 0; i < array.length; i++) {
-    var line = "";
-    for (var index in array[i]) {
-      if (line != "") line += ",";
-
-      line += array[i][index];
-    }
-
-    str += line + "\r\n";
-  }
-
-  return str;
-}
-
-let URL =
-  "https://www.apartments.com/apartments/max-2-bedrooms-pet-friendly-dog/washer-dryer-parking-ev-charging/?bb=pi94mtom-Ho7i1j4B";
-let apartmentURLs = await getURLs(URL);
-
-//for each apartment id, get the data specified
-
-let allData = [];
-
-await Promise.all(
-  apartmentURLs.map(async (URL) => allData.push(await getData(URL)))
-);
-
-allData = allData.flat(3);
-console.table(allData);
-
-stringify(
-  allData, 
-  {
-    header: true
-  }, 
-  function(err, output) {
-  fs.writeFile('apartmentDetails.csv', output, 'utf8', () => console.log('done.'));
+const consoleRead = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
 })
 
+async function main(searchURL) {
+	let apartmentURLs = await getURLs(searchURL)
+
+	console.log('looking at these URLs:')
+
+	console.log(apartmentURLs)
+	//for each apartment id, get the data specified
+
+	process.setMaxListeners(Infinity)
+
+	let allData = []
+
+	await Promise.all(
+		apartmentURLs.map(async (apartmentURL) =>
+			allData.push(await getData(apartmentURL))
+		)
+	)
+
+	allData = allData.flat(3)
+
+	stringify(
+		allData,
+		{
+			header: true,
+		},
+		function (err, output) {
+			fs.writeFile('apartmentDetails.csv', output, 'utf8', () =>
+				console.log('wrote data to ./apartmentDetails.csv')
+			)
+		}
+	)
+}
+
+//starting thread
+consoleRead.question('enter url of apartments.com search\n', (input) => {
+	main(input).then(() => consoleRead.close())
+})
